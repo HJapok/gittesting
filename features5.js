afcab581ffffb2e5c36b30f870b59cd46fef459d
HaroldJapok <114495302+HaroldJapok@users.noreply.github.com>
2022-10-10 16:20:13 +0800

    add div in featues 5

--- features5.js
+++ features5.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export class features5 extends Component {
   render() {
     return (
-      <div>features5</div>
+      <div>features5
+        <div>features5</div>
+        <div>features5</div>
+      </div>
+      
     )
   }
 }
